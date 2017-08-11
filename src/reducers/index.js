7f69b91b16b702b5b742295d93394c4a45def030
choniawko <pawel.choniawko@gmail.com>
2017-08-11 10:03:06 +0200

    reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import * as consts from '../actions/constans';
+const initialState = [];
+
+function TaskReducer (state = initialState, action) {
+    switch(action.type) {
+        case consts.ADD_TASK:
+            return [
+                ...state,
+                {
+                    id: action.payload.id,
+                    name: action.payload.name,
+                    done: false
+                }
+            ]
+        case consts.TOGGLE_TASK:
+            return state.map((task) => 
+            (task.id === action.payload.id) ? { ...task, done: !task.done } : task)
+    }
+}
+
+export default TaskReducer;
